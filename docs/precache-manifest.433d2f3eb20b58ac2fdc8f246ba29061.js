449dc2f8ed0a913733bee7a5f4a3d1afadd8f087
Ilia Grigorev <ru.igsoft@gmail.com>
2020-07-20 13:36:08 +0200

    Demo v0.2.0

---
+++ docs/precache-manifest.433d2f3eb20b58ac2fdc8f246ba29061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba2f86f8233d59a33e7bf55fb0424fa",
+    "url": "/twistyeditor/index.html"
+  },
+  {
+    "revision": "8bf79dd377490d7d549c",
+    "url": "/twistyeditor/static/css/main.e4d0ed32.chunk.css"
+  },
+  {
+    "revision": "c84e44da1d9f716e1e76",
+    "url": "/twistyeditor/static/js/2.557a44cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/twistyeditor/static/js/2.557a44cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf79dd377490d7d549c",
+    "url": "/twistyeditor/static/js/main.a6744e2b.chunk.js"
+  },
+  {
+    "revision": "35a6e230d249a4af6364",
+    "url": "/twistyeditor/static/js/runtime-main.7b0c9671.js"
+  }
+]);
