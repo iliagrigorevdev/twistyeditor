836070bd9b2022ae0093819c0fd14b167de211c8
Ilia Grigorev <ru.igsoft@gmail.com>
2021-06-15 21:47:35 +0300

    Link standalone program to prebuilt LibTorch libraries

--- training/compile_standalone.sh
+++ training/compile_standalone.sh
@@ -1,8 +1,4 @@
-cd extern/pytorch
-$(pwd)/scripts/build_host_protoc.sh
-cd ../..
-
-cmake -B build_standalone -DPYTHON_EXECUTABLE="$(which python3)"
+cmake -B build_standalone -DCMAKE_PREFIX_PATH=../../libtorch/share/cmake/Torch
 cd build_standalone
 make -j 4
 cd ..
