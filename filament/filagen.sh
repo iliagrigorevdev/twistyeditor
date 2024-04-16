3afac12603bb526d599025aefcb890654f6da04b
Ilia Grigorev <ru.igsoft@gmail.com>
2024-04-16 11:03:33 +0200

    Update Filament to latest version 1.51.4

--- filament/filagen.sh
+++ filament/filagen.sh
@@ -4,7 +4,7 @@ filamesh --compress prism.obj ../public/res/prism.filamesh
 filamesh --compress knob.obj ../public/res/knob.filamesh
 filamesh --compress section.obj ../public/res/section.filamesh
 
-matc -a opengl -p mobile -o ../public/res/prism.filamat prism.mat
-matc -a opengl -p mobile -o ../public/res/ghost.filamat ghost.mat
-matc -a opengl -p mobile -o ../public/res/ground.filamat ground.mat
-matc -a opengl -p mobile -o ../public/res/highcol.filamat highcol.mat
+matc -a opengl -p mobile -V skinning,vsm,ssr,stereo -o ../public/res/prism.filamat prism.mat
+matc -a opengl -p mobile -V skinning,vsm,ssr,stereo -o ../public/res/ghost.filamat ghost.mat
+matc -a opengl -p mobile -V skinning,vsm,ssr,stereo -o ../public/res/ground.filamat ground.mat
+matc -a opengl -p mobile -V skinning,vsm,ssr,stereo -o ../public/res/highcol.filamat highcol.mat
