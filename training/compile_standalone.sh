a84fc97e5e1ae8b2425e9ddbb04ac4eac6efe421
Ilia Grigorev <ru.igsoft@gmail.com>
2024-05-29 14:33:25 +0200

    Build Training with CUDA (with workaround TORCH_CUDA_ARCH_LIST)

--- training/compile_standalone.sh
+++ training/compile_standalone.sh
@@ -4,7 +4,9 @@ else
   MAX_JOBS=$(nproc)
 fi
 
-cmake -B build_standalone -DCMAKE_PREFIX_PATH=../../libtorch/share/cmake/Torch
+export PATH=$PATH:/usr/local/cuda/bin
+export TORCH_CUDA_ARCH_LIST="8.0 8.6 8.9 9.0"
+cmake -DCMAKE_BUILD_TYPE=Release -B build_standalone -DCMAKE_PREFIX_PATH=~/Documents/projects/tools/libtorch_cuda/share/cmake/Torch
 cd build_standalone
 make "-j${MAX_JOBS}"
 cd ..
