9f56fab49c2bebb4a717a3290f57d4b2a0645ed4
Ilia Grigorev <ru.igsoft@gmail.com>
2024-05-30 09:57:27 +0200

    Fix compile_standalone.sh

--- training/compile_standalone.sh
+++ training/compile_standalone.sh
@@ -4,9 +4,7 @@ else
   MAX_JOBS=$(nproc)
 fi
 
-export PATH=$PATH:/usr/local/cuda/bin
-export TORCH_CUDA_ARCH_LIST="8.0 8.6 8.9 9.0"
-cmake -DCMAKE_BUILD_TYPE=Release -B build_standalone -DCMAKE_PREFIX_PATH=~/Documents/projects/tools/libtorch_cuda/share/cmake/Torch
+cmake -DCMAKE_BUILD_TYPE=Release -B build_standalone
 cd build_standalone
 make "-j${MAX_JOBS}"
 cd ..
